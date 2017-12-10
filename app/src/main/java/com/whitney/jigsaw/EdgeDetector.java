312b6c5c4c2298d74b10829927c012a06ef7186c
Whitney <whitney.babcockmcconnell@gmail.com>
2017-12-10 04:11:38 -0500

    Attempting to use the edge detector on a source image and output to an ImageView... my output is all white

--- app/src/main/java/com/whitney/jigsaw/EdgeDetector.java
+++ app/src/main/java/com/whitney/jigsaw/EdgeDetector.java
@@ -1,22 +1,86 @@
 package com.whitney.jigsaw;
 
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v4.graphics.ColorUtils;
+import android.widget.TextView;
 
 /**
  * Created by Dracle on 12/9/2017.
  */
 
-public class EdgeDetector {
+public class EdgeDetector
+{
+//    // truncate color component to be between 0 and 255
+//    public static int truncate(int a) {
+//        if (a < 0) return 0;
+//        else if (a > 255) return 255;
+//        else return a;
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        int[][] filter1 = {
+//                {-1, 0, 1},
+//                {-2, 0, 2},
+//                {-1, 0, 1}
+//        };
+//
+//        int[][] filter2 = {
+//                {1, 2, 1},
+//                {0, 0, 0},
+//                {-1, -2, -1}
+//        };
+//
+//        Picture picture0 = new Picture(args[0]);
+//        int width = picture0.width();
+//        int height = picture0.height();
+//        Picture picture1 = new Picture(width, height);
+//
+//
+//        for (int y = 1; y < height - 1; y++) {
+//            for (int x = 1; x < width - 1; x++) {
+//
+//                // get 3-by-3 array of colors in neighborhood
+//                int[][] gray = new int[3][3];
+//                for (int i = 0; i < 3; i++) {
+//                    for (int j = 0; j < 3; j++) {
+//                        gray[i][j] = (int) Luminance.intensity(picture0.get(x - 1 + i, y - 1 + j));
+//                    }
+//                }
+//
+//                // apply filter
+//                int gray1 = 0, gray2 = 0;
+//                for (int i = 0; i < 3; i++) {
+//                    for (int j = 0; j < 3; j++) {
+//                        gray1 += gray[i][j] * filter1[i][j];
+//                        gray2 += gray[i][j] * filter2[i][j];
+//                    }
+//                }
+//                // int magnitude = 255 - truncate(Math.abs(gray1) + Math.abs(gray2));
+//                int magnitude = 255 - truncate((int) Math.sqrt(gray1 * gray1 + gray2 * gray2));
+//                Color grayscale = new Color(magnitude, magnitude, magnitude);
+//                picture1.set(x, y, grayscale);
+//            }
+//        }
+//        picture0.show();
+//        picture1.show();
+//        // picture1.save("baboon-edge.jpg");
+//    }
+
     // truncate color component to be between 0 and 255
-    public static int truncate(int a) {
+    private static int truncate(int a)
+    {
         if (a < 0) return 0;
-        else if (a > 255) return 255;
-        else return a;
+        if (a > 255) return 255;
+        return a;
     }
 
-    public static void main(String[] args) {
-
+    public static Bitmap sobel(Resources resources, int imageResId, final TextView progressView, Activity activity)
+    {
         int[][] filter1 = {
                 {-1, 0, 1},
                 {-2, 0, 2},
@@ -29,39 +93,56 @@ public class EdgeDetector {
                 {-1, -2, -1}
         };
 
-        Picture picture0 = new Picture(args[0]);
-        int width = picture0.width();
-        int height = picture0.height();
-        Picture picture1 = new Picture(width, height);
+        Bitmap picture0 = BitmapFactory.decodeResource(resources, imageResId);
+        int width = picture0.getWidth();
+        int height = picture0.getHeight();
+        Bitmap picture1 = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 
-
-        for (int y = 1; y < height - 1; y++) {
-            for (int x = 1; x < width - 1; x++) {
-
+        for (int y = 1; y < height - 1; y++)
+        {
+            for (int x = 1; x < width - 1; x++)
+            {
                 // get 3-by-3 array of colors in neighborhood
                 int[][] gray = new int[3][3];
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        gray[i][j] = (int) Luminance.intensity(picture0.get(x - 1 + i, y - 1 + j));
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+//                        gray[i][j] = (int) Luminance.intensity(picture0.get(x - 1 + i, y - 1 + j));
+                        gray[i][j] = (int) ColorUtils.calculateLuminance(picture0.getPixel(x - 1 + i, y - 1 + j));
                     }
                 }
 
                 // apply filter
                 int gray1 = 0, gray2 = 0;
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
                         gray1 += gray[i][j] * filter1[i][j];
                         gray2 += gray[i][j] * filter2[i][j];
                     }
                 }
                 // int magnitude = 255 - truncate(Math.abs(gray1) + Math.abs(gray2));
                 int magnitude = 255 - truncate((int) Math.sqrt(gray1 * gray1 + gray2 * gray2));
-                Color grayscale = new Color(magnitude, magnitude, magnitude);
-                picture1.set(x, y, grayscale);
+                int grayscale = Color.argb(255, magnitude, magnitude, magnitude);
+                picture1.setPixel(x, y, grayscale);
+
+
+                final float percent = 100.0f * (float) ((y * width) + x) / (float) (height * width);
+                activity.runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        progressView.setText("" + percent);
+                    }
+                });
             }
         }
-        picture0.show();
-        picture1.show();
-        // picture1.save("baboon-edge.jpg");
+
+        progressView.setText("Done");
+
+        return picture1;
     }
 }
--- app/src/main/java/com/whitney/jigsaw/MainActivity.kt
+++ app/src/main/java/com/whitney/jigsaw/MainActivity.kt
@@ -2,11 +2,25 @@ package com.whitney.jigsaw
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var resultImageView: ImageView? = null
+    var buttonTextView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultImageView = findViewById(R.id.result_imageview)
+        buttonTextView = findViewById(R.id.button_textview);
+        buttonTextView?.setOnClickListener({
+            buttonTextView?.setOnClickListener(null)
+
+            val bitmap = EdgeDetector.sobel(resources, R.drawable.jigsaw_puzzle_01, buttonTextView, this)
+            resultImageView?.setImageBitmap(bitmap)
+        })
     }
 }
